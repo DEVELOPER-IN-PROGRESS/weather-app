a8dfda359a41643e9fa299c23e7f0d105eaec06d
iceweasel <johnsjosephk@pm.me>
2025-03-19 23:47:23 +0530

    responsive styling complete , data fetched to ui

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let  form_overlay = document.getElementById('data-submit')
 let light_mode = true
 
 function set_background(hours){
-    console.log('inside');
-    document.body.style.backgroundImage = hours >= 12 ?
+    console.log({hours});
+    // document.body.style.backgroundImage =  ( hours >= 8 && hours <= 18 )?  //light mode
     // `url('https://plus.unsplash.com/premium_photo-1672070779337-e5655d9bbd9e?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
-    `url('https://images.unsplash.com/photo-1541119638723-c51cbe2262aa?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnJpZ2h0JTIwc2t5fGVufDB8fDB8fHww')`
-    :
-    `url('https://images.unsplash.com/photo-1504858700536-882c978a3464?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
+    // `url('https://images.unsplash.com/photo-1541119638723-c51cbe2262aa?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnJpZ2h0JTIwc2t5fGVufDB8fDB8fHww')`
+    // :
+    // `url('https://images.unsplash.com/photo-1504858700536-882c978a3464?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
     // `url('http://plus.unsplash.com/premium_photo-1727730047398-49766e915c1d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xlYXIlMjBza3l8ZW58MHx8MHx8fDA%3D')`;
 }
 
@@ -47,42 +47,39 @@ async function fetch_weather_data(){
 
     try{
         if( data.cod == 200 ){
-            content.innerHTML = `
-                 <div class="ps-5 md:col-span-2">
-                <p class="location text-lg">
-                    <i class="fa-solid fa-location-dot "></i>
-                    <span id="place"></span>
-                </p>
-                <p class="location text-lg">
-                    <span id="temperature"></span>
-                </p>
+            // content.innerHTML = `
+            //      <div class="ps-5 md:col-span-2">
+            //     <p class="location text-lg">
+            //         <i class="fa-solid fa-location-dot "></i>
+            //         <span id="place"></span>
+            //     </p>
+            //     <p class="location text-lg">
+            //         <span id="temperature"></span>
+            //     </p>
 
-            </div>
-            <div class="ps-5 md:col-span-2">
-                <p class="location text-lg">
-                    Humidity
-                    <span id="humidity"></span>
-                </p>
+            // </div>
+            // <div class="ps-5 md:col-span-2">
+            //     <p class="location text-lg">
+            //         Humidity
+            //         <span id="humidity"></span>
+            //     </p>
 
-                <p class="">
-                    Wind Speed
-                    <span id="wind"></span>
-                </p>
+            //     <p class="">
+            //         Wind Speed
+            //         <span id="wind"></span>
+            //     </p>
 
-                <p class="">
-                    Pressure
-                    <span id="pressure"></span>
-                </p>
+            //     <p class="">
+            //         Pressure
+            //         <span id="pressure"></span>
+            //     </p>
 
+            //     <p class="">
+            //         <span id="date"></span>
+            //     </p>
 
-                <p class="">
-                    <span id="date"></span>
-                </p>
-
-
-            </div>
-            `
-
+            // </div>
+            // `
             let date = new Date((data.dt + data.timezone) * 1000)
             let day = date.getUTCDate();
             let month = date.getUTCMonth() + 1;
@@ -90,23 +87,59 @@ async function fetch_weather_data(){
             let hours = date.getUTCHours();
             let minutes = date.getUTCMinutes();
             let meridiem = hours >= 12 ? 'PM' : 'AM';
+
             set_background(hours)
+
+            // light_mode = (hours >= 8 && hours <= 18)? false : true ;
+            // changeMode()
+
+            // 
+
             hours = (hours % 12) || 12;
             month = month.toString().padStart(2,'0')
             day = day.toString().padStart(2,'0')
             minutes = minutes.toString().padStart(2,'0')
-            document.getElementById('date').innerText =  `${day}-${month}-${year} ${hours}:${minutes} ${meridiem}`;
+            //   debugger;
+            content.innerHTML = `
+            <div class="ps-5 md:ps-15 md:col-span-2">
+                <p class="location text-lg">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <span id="place"> ${data.name}, ${data.sys.country}</span>
+                </p>
+                <p class="location text-lg">
+                    <span id="temperature"> ${ (data.main.temp - 273.15).toFixed(2) }°C</span>
+                </p>
+                <p class="">
+                    <i class="fa-solid fa-temperature-low"></i>
+                    Feels Like: ${ (data.main.feels_like - 273.15).toFixed(2) }°C
+                </p>
 
-            humidity.innerText = `${data.main.humidity} %` ;
-            wind.innerText = `${data.wind.speed} m/s`;
-            pressure.innerText = `${data.main.pressure} hPa`;
-            place.innerText = `${data.name}, ${data.sys.country}`;
-            temperature.innerText = `${data.main.temp}°C`;
+                <h6> Weather: ${data.weather[0].main} </h6>
+                <span> ${data.weather[0].description} </span>
+                <p class="mt-2"> ${day}-${month}-${year} <br> ${hours}:${minutes} ${meridiem} </p>
+
+            </div>
+            <div class="ps-5 md:col-span-2">
+                <p class="location ">
+                    <i class="fas fa-tint"></i>
+                    Humidity
+                    <span id="humidity"> ${data.main.humidity} % </span>
+                </p>
+
+                <p class="">
+                    <i class="fas fa-wind"></i>
+                    Wind Speed
+                    <span id="wind"> ${data.wind.speed} m/s </span>
+                </p>
+                <p class="">
+                    <i class="fas fa-tachometer-alt"></i>
+                    Pressure
+                    <span id="pressure"> ${data.main.pressure} hPa </span>
+                </p>
+            </div>
+            `
 
             /*
-
-
-
              weather
              celcius            weather.main.temp
              feels like         weather.main.feels_like
@@ -121,8 +154,6 @@ async function fetch_weather_data(){
             weather.coord : object weather.coord   .lat/lon
             weather.name
             */
-
-
         }
     }
     catch( error){
@@ -132,8 +163,12 @@ async function fetch_weather_data(){
     city.value = '';
 }
 
-
 form_overlay.addEventListener("submit", (e) => {e.preventDefault(); fetch_weather_data()})
-window.addEventListener("DOMContentLoaded", set_background())
-toggle.addEventListener('click' , changeMode )
 
+window.addEventListener("DOMContentLoaded",  () => {
+    let d = new Date().getHours()
+    // set_background(d)
+    // if its only evening hours change the initial light to dark mode
+    !(d >= 8 && d <= 18) && changeMode()
+})
+toggle.addEventListener('click' , changeMode )
